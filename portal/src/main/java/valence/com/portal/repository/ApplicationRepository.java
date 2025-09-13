60986804ca37e2c39fcffb85b0963757ad826a24
PHENOMVALENCE <phenomenalvalence@gmail.com>
2025-09-13 10:10:28 +0300

    clean

--- portal/src/main/java/valence/com/portal/repository/ApplicationRepository.java
+++
@@ -1,8 +0,0 @@
-package valence.com.portal.repository;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class ApplicationRepository extends JpaRepository<Application, Integer> {
-    
-}
--- portal/src/main/java/valence/com/portal/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package valence.com.portal.repository;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class StudentRepository extends JpaRepository<Student, Integer> {
-
-    
-}
